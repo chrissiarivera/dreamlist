c2b253873ff72ccee90fb6567eff79567803dd0a
chrissiarivera <riverajalane@gmail.com>
2017-10-11 19:54:11 +0800

    first commit

---
+++ app/src/main/java/jalanechrissia/rivera/com/riveraceniza/Dream.java
@@ -0,0 +1,61 @@
+package jalanechrissia.rivera.com.riveraceniza;
+
+/**
+ * Created by Jalane Chrissia on 11/10/2017.
+ */
+
+public class Dream {
+    private int id;
+    private String name;
+    private String price;
+    private String desc;
+    private byte[] image;
+
+    public Dream(int id, String name, String price, String desc, byte[] image) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.desc = desc;
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+}
---
+++ app/src/main/java/jalanechrissia/rivera/com/riveraceniza/displayDesc.java
@@ -0,0 +1,13 @@
+package jalanechrissia.rivera.com.riveraceniza;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class displayDesc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_desc);
+    }
+}
